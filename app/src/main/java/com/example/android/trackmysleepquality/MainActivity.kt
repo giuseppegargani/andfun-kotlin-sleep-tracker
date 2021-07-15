50be66170b2434070d7099134bb9b45ff59309a9
Houde Giuseppe Gargani <sentierodisole1@yahoo.it>
2021-07-16 01:49:58 +0200

    Terza Commit

--- app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
@@ -45,21 +45,26 @@ import androidx.appcompat.app.AppCompatActivity
    viewModelJob.cancel()
     }
  */
+
 /*TODO - 2 Definisci uno Scope per le coroutines
     private val uiScope = CoroutineScope(Dispatchers.Main +  viewModelJob)
  */
+
 /*TODO - 3 Definisci una variabile Tonight che tiene il valore corrente
     Deve essere MutableLiveData
     private var tonight = MutableLiveData<SleepNight?>()
  */
+
 /*TODO - 4 Definisci una variabile ed assegna il valore di tutte le notti
     private val nights = database.getAllNights()
  */
+
 /*TODO - 5 Per inizializzare crea un blocco init ed assegnali un metodo
     init {
     initializeTonight()
     }
  */
+
 /*TODO - 6 Implementa InitializeTonight
     In UIscope lancia una Coroutine
     private fun initializeTonight() {
@@ -68,13 +73,16 @@ import androidx.appcompat.app.AppCompatActivity
      }
     }
  */
+
 /*TODO - 7 Implementa GetTonightFromDatabase
     Definisci una private suspend function che restituisce un nullable SleepNight se non c'è nessun corrent sleepNight
     private suspend fun getTonightFromDatabase():  SleepNight? { }
  */
+
 /*TODO - 8  Restituisci il risultato da una coroutine che esegue nel context Dispatcher.Io
     return withContext(Dispatchers.IO) {}
  */
+
 /*TODO - 9 Lascia che la coroutine prenda tonight dal Database
     Se il tempo iniziale e finale non sono uguale restituisci null, altrimenti restituisci night
     var night = database.getTonight()
@@ -97,6 +105,7 @@ import androidx.appcompat.app.AppCompatActivity
 /*TODO - 11 IMPOSTA TONIGHT COME NUOVA NOTTE
     tonight.value = getTonightFromDatabase()
  */
+
 /*TODO - 12 Crea una funzione Insert che lancia una coroutine nel IoContext e inserisce night nel DB
     withContext(Dispatchers.IO) {
     database.insert(night)
@@ -113,6 +122,7 @@ import androidx.appcompat.app.AppCompatActivity
     }
     }
  */
+
 /*TODO - 14 IMPLEMENTA update() usando lo stesso pattern di Insert()
     private suspend fun update(night: SleepNight) {
     withContext(Dispatchers.IO) {
@@ -120,6 +130,7 @@ import androidx.appcompat.app.AppCompatActivity
     }
     }
  */
+
 /*TODO - 15 Implementa analogamente OnClear e Clear(
     fun onClear() {
     uiScope.launch {
@@ -133,14 +144,17 @@ import androidx.appcompat.app.AppCompatActivity
     }
     }
  */
+
 /*TODO - 16 Aggiungi gli handlers ai tre pulsanti nei files XML
     android:onClick="@{() ->  sleepTrackerViewModel.onStartTracking()}"
  */
+
 /*TODO - 17 Formatta il testo di nights usando la funzione formatNights()
     val nightsString = Transformations.map(nights) { nights ->
     formatNights(nights, application.resources)
     }
  */
+
 /*TODO - 18 fai gli opportuni collegamenti in Fragment_sleep_tracker
     "@{sleepTrackerViewModel.nightsString}"
  */
